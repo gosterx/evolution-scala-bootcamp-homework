9080b486d69a2571b985102d5e8054664e82e818
gosterx <gosterxgames@gmail.com>
2021-03-22 22:08:38 +0300

    ScalaExercises homework supplemented

--- src/test/scala/com/evolution/bootcamp/homework/cats/ScalaExercisesSpec.scala
+++ src/test/scala/com/evolution/bootcamp/homework/cats/ScalaExercisesSpec.scala
@@ -201,4 +201,62 @@ class ScalaExercisesSpec extends AnyFunSuite{
     val fortytwo: Int = 42
     Comonad[Id].coflatMap(fortytwo)(_ + 1) should be(43)
   }
+  test("Either") {
+    val right: Either[String, Int] = Either.right(5)
+    right.map(_ + 1) should be(Right(6))
+
+    val left: Either[String, Int] = Either.left("Something went wrong")
+    left.map(_ + 1) should be(Left("Something went wrong"))
+
+    val right1: Either[String, Int] = Either.right(5)
+    right1.flatMap(x => Either.right(x + 1)) should be(Right(6))
+
+    val left1: Either[String, Int] = Either.left("Something went wrong")
+    left1.flatMap(x => Either.right(x + 1)) should be(Left("Something went wrong"))
+
+    object EitherStyle {
+      def parse(s: String): Either[NumberFormatException, Int] =
+        if (s.matches("-?[0-9]+")) Either.right(s.toInt)
+        else Either.left(new NumberFormatException(s"${s} is not a valid integer."))
+
+      def reciprocal(i: Int): Either[IllegalArgumentException, Double] =
+        if (i == 0) Either.left(new IllegalArgumentException("Cannot take reciprocal of 0."))
+        else Either.right(1.0 / i)
+
+      def stringify(d: Double): String = d.toString
+
+      def magic(s: String): Either[Exception, String] =
+        parse(s).flatMap(reciprocal).map(stringify)
+    }
+
+    EitherStyle.parse("Not a number").isRight should be(false)
+    EitherStyle.parse("2").isRight should be(true)
+
+    import EitherStyle._
+
+    magic("0").isRight should be(false)
+    magic("1").isRight should be(true)
+    magic("Not a number").isRight should be(false)
+
+    val result = magic("2") match {
+      case Left(_: NumberFormatException) => "Not a number!"
+      case Left(_: IllegalArgumentException) => "Can't take reciprocal of 0!"
+      case Left(_) => "Unknown error"
+      case Right(result) => s"Got reciprocal: ${result}"
+    }
+    result should be("Got reciprocal: 0.5")
+
+    val right3: Either[String, Int] = Right(41)
+    right3.map(_ + 1) should be(Right(42))
+
+    val left3: Either[String, Int] = Left("Hello")
+    left3.map(_ + 1) should be(Left("Hello"))
+    left3.leftMap(_.reverse) should be(Left("olleH"))
+
+    Either.catchOnly[NumberFormatException]("abc".toInt).isRight should be(false)
+    Either.catchNonFatal(1 / 0).isLeft should be(true)
+
+    val right4: Either[String, Int] = 42.asRight[String]
+    right4 should be(Right(42))
+  }
 }
