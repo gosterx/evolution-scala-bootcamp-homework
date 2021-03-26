065c33b5f55ee61b9f2fc0f4e5552e6e5d33333d
gosterx <gosterxgames@gmail.com>
2021-03-26 14:09:42 +0300

    ErrorHandling homework fixed

--- src/main/scala/com/evolution/bootcamp/homework/error_handling/ErrorHandling.scala
+++ src/main/scala/com/evolution/bootcamp/homework/error_handling/ErrorHandling.scala
@@ -2,6 +2,8 @@ package com.evolution.bootcamp.homework.error_handling
 
 import cats.data.ValidatedNec
 import cats.syntax.all._
+import com.evolution.bootcamp.homework.error_handling.ErrorHandling.Homework.PaymentCard.AllErrorsOr
+import com.evolution.bootcamp.homework.error_handling.ErrorHandling.Homework.ValidationError.{ExpirationDateFormatError, ExpirationDateMonthIsNotNumeric, ExpirationDateMonthIsNotValid, ExpirationDateYearIsNotNumeric}
 
 object ErrorHandling {
 
@@ -11,68 +13,39 @@ object ErrorHandling {
   // 2. Add `ValidationError` cases (at least 5, may be more).
   // 3. Implement `validate` method to construct `PaymentCard` instance from the supplied raw data.
   object Homework {
-    case class ExpirationDate(month: Int, year: Int)
-    case class PaymentCard(name: String,
-                           number: Int,
-                           expirationDate: ExpirationDate,
-                           securityCode: Int)
-
     sealed trait ValidationError
     object ValidationError {
-      final case object EmptyNameError extends ValidationError {
+      case object EmptyNameError extends ValidationError {
         override def toString: String = "Name can't be empty"
       }
-      final case object NumberIsNotNumeric extends ValidationError {
+      case object NumberIsNotNumeric extends ValidationError {
         override def toString: String = "Number must be a number"
       }
-      final case object NumberLengthError extends ValidationError {
+      case object NumberLengthError extends ValidationError {
         override def toString: String = "The number must be 16 digits"
       }
-      final case object ExpirationDateFormatError extends ValidationError {
+      case object ExpirationDateFormatError extends ValidationError {
         override def toString: String = "Expiration date format is invalid(must be xx/xx)"
       }
-      final case object ExpirationDateMonthIsNotNumeric extends ValidationError {
+      case object ExpirationDateMonthIsNotNumeric extends ValidationError {
         override def toString: String = "Month of expiration date must be a number"
       }
-      final case object ExpirationDateMonthIsNotValid extends ValidationError {
+      case object ExpirationDateMonthIsNotValid extends ValidationError {
         override def toString: String = "Month of expiration date must be a number from 1 to 12"
       }
-      final case object ExpirationDateYearIsNotNumeric extends ValidationError {
+      case object ExpirationDateYearIsNotNumeric extends ValidationError {
         override def toString: String = "Year of expiration date must be a number"
       }
-      final case object SecurityCodeIsNotNumeric extends ValidationError {
+      case object SecurityCodeIsNotNumeric extends ValidationError {
         override def toString: String = "Security code must be a number"
       }
-      final case object SecurityCodeLengthError extends ValidationError {
+      case object SecurityCodeLengthError extends ValidationError {
         override def toString: String = "Security code must be 3 digits"
       }
     }
 
-    object PaymentCardValidator {
-
-      type AllErrorsOr[A] = ValidatedNec[ValidationError, A]
-
-      import ValidationError._
-
-      private def validateName(name: String): AllErrorsOr[String] =
-        if (name.nonEmpty) name.validNec
-        else EmptyNameError.invalidNec
-
-      private def validateNumber(number: String): AllErrorsOr[Int] = {
-
-        def validateNumberIsNotNumeric: AllErrorsOr[Int] = number.toIntOption match {
-          case Some(x) => x.validNec
-          case None    => NumberIsNotNumeric.invalidNec
-        }
-
-        def validateNumberLengthError(number: Int): AllErrorsOr[Int] =
-          if (number.toString.length == 16) number.validNec
-          else NumberLengthError.invalidNec
-
-        validateNumberIsNotNumeric andThen validateNumberLengthError
-      }
-
-      // xx/xx
+    sealed abstract case class ExpirationDate private (month: Int, year: Int)
+    object ExpirationDate {
       private def validateExpirationDate(expirationDate: String): AllErrorsOr[ExpirationDate] = {
 
         def validateExpirationDateFormatError: AllErrorsOr[String] =
@@ -97,32 +70,66 @@ object ErrorHandling {
 
         validateExpirationDateFormatError.andThen(_ =>
           (validateExpirationDateMonthIsNotNumeric andThen validateExpirationDateMonth, validateExpirationDateYearIsNotNumeric)
-            .mapN(ExpirationDate))
+            .mapN((date, month) => new ExpirationDate(date, month) {}))
       }
 
-      private def validateSecurityCode(securityCode: String): AllErrorsOr[Int] = {
+      def create(expirationDate: String): AllErrorsOr[ExpirationDate] = validateExpirationDate(expirationDate)
+    }
 
-        def validateSecurityCodeIsNotNumeric: AllErrorsOr[Int] = securityCode.toIntOption match {
-          case Some(x) => x.validNec
+    sealed abstract case class PaymentCard private (name: String,
+                           number: String,
+                           expirationDate: ExpirationDate,
+                           securityCode: String)
+    object PaymentCard {
+      type AllErrorsOr[A] = ValidatedNec[ValidationError, A]
+
+      import ValidationError._
+
+      private def validateName(name: String): AllErrorsOr[String] =
+        if (name.nonEmpty) name.validNec
+        else EmptyNameError.invalidNec
+
+      private def validateNumber(number: String): AllErrorsOr[String] = {
+        def validateNumberIsNotNumeric: AllErrorsOr[String] = number.toIntOption match {
+          case Some(x) => x.toString.validNec
+          case None    => NumberIsNotNumeric.invalidNec
+        }
+
+        def validateNumberLengthError(number: String): AllErrorsOr[String] =
+          if (number.length == 16) number.validNec
+          else NumberLengthError.invalidNec
+
+        validateNumberIsNotNumeric andThen validateNumberLengthError
+      }
+
+      private def validateSecurityCode(securityCode: String): AllErrorsOr[String] = {
+        def validateSecurityCodeIsNotNumeric: AllErrorsOr[String] = securityCode.toIntOption match {
+          case Some(x) => x.toString.validNec
           case None    => SecurityCodeIsNotNumeric.invalidNec
         }
 
-        def validateSecurityCodeLengthError(securityCode: Int): AllErrorsOr[Int] =
-          if (securityCode.toString.length == 16) securityCode.validNec
+        def validateSecurityCodeLengthError(securityCode: String): AllErrorsOr[String] =
+          if (securityCode.length == 3) securityCode.validNec
           else SecurityCodeLengthError.invalidNec
 
         validateSecurityCodeIsNotNumeric andThen validateSecurityCodeLengthError
       }
 
-      def validate(
+      private def validate(
                     name: String,
                     number: String,
                     expirationDate: String,
                     securityCode: String,
                   ): AllErrorsOr[PaymentCard] =
-        (validateName(name), validateNumber(number), validateExpirationDate(expirationDate),validateSecurityCode(securityCode))
-          .mapN(PaymentCard)
+        (validateName(name), validateNumber(number), ExpirationDate.create(expirationDate),validateSecurityCode(securityCode))
+          .mapN((name, number, expirationDate, securityCode) => new PaymentCard(name, number, expirationDate, securityCode) {})
+
+      def create(
+                  name: String,
+                  number: String,
+                  expirationDate: String,
+                  securityCode: String,
+                ): AllErrorsOr[PaymentCard] = validate(name, number, expirationDate, securityCode)
     }
   }
-
 }
